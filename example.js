fa8bb5fad9f3689a683783730288949d02aa0846
Haslien <40437596+KeyboardRage@users.noreply.github.com>
2023-04-14 17:31:17 +0200

    Createn example use file

---
+++ example.js
@@ -0,0 +1,35 @@
+/* 
+Example how to use the brute-forcer
+*/
+const BruteForcer = require("./BruteForcer.js");
+
+// Async IIFE to allow for 'await'
+(async () => {
+  // Initialize with the LAN IP of the pritner
+  const forcer = new BruteForcer("192.168.1.96", {log: true, logPassword: true});
+
+  // Our list of usernames
+  const usernames = [
+    "admin",
+    "Admin",
+    "epson_admin",
+    "epsonweb",
+    "administrator",
+    "Administrator",
+  ];
+
+  // Possible passwords
+  const passwords = [
+    "admin",
+    "epson_admin",
+    "administrator",
+    "12345",
+    "123456789",
+  ];
+
+  // Start cracking provided username/password list
+  const found = await forcer.crack(usernames, passwords);
+
+  if (!found) console.log("No matches");
+  else console.log(found); // {username: string, password: string}
+})();
